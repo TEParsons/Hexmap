b4426403289dc090f999eede8745d47a65f1d26a
Todd Parsons <todd.e.parsons@googlemail.com>
2022-10-29 14:33:04 +0100

    Modularise map loading

--- hex.js
+++ hex.js
@@ -84,6 +84,10 @@ class HexGrid extends HTMLElement {
         tile.type = "ocean";
       }
     }
+
+    if (this.dataset['tiles']) {
+      importMap(this.dataset['tiles'])
+    }
   }
 
   export() {
@@ -247,3 +251,36 @@ class IconOption extends HTMLElement {
   }
 }
 customElements.define("icon-option", IconOption);
+
+
+function exportMap() {
+  let map = document.getElementById("map");
+  // Get tile values
+  let tiles = map.export()
+  let tilesCSV = tiles.join(",")
+
+  // Save to file
+  let file = new Blob([tilesCSV], { type: "text/csv" });
+  window.open(URL.createObjectURL(file));
+
+}
+
+function loadFile(file) {
+  let reader = new FileReader();
+  reader.addEventListener("loadend", function () {
+    let raw = reader.result;
+    map.import(raw.split(","));
+  })
+  reader.readAsText(file);
+}
+
+function importMap(tiles) {
+  let map = document.getElementById("map");
+  let loadbuffer = document.getElementById("loadbuffer");
+  // Load the .csv file to a list
+  loadbuffer.onchange = function () {
+    loadFile(loadbuffer.files[0]);
+  };
+  // Click to trigger previously defined functions
+  loadbuffer.click();
+}
